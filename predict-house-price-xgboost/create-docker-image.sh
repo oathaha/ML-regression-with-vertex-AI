091b92abb57ab63d8c41739d9ca95c9245b54408
Oat <windowsandandroid.app@gmail.com>
2024-10-24 14:46:29 +1100

    prepare file for building CI/CD pipeline

--- predict-house-price-xgboost/create-docker-image.sh
+++ predict-house-price-xgboost/create-docker-image.sh
@@ -1,5 +1,5 @@
 PROJECT_ID='house-price-prediction-439306'
-IMAGE_URI="gcr.io/$PROJECT_ID/xgb-model:hypertune"
+IMAGE_URI="gcr.io/$PROJECT_ID/xgb-model:deployment"
 
 gcloud auth configure-docker
 docker build ./ -t $IMAGE_URI
